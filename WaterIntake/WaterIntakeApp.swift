824756d52af19468d9d5b009c04bb2331cdbc0e5
Ashish Prajapati <>
2025-06-08 16:42:11 +0530

    test commit

--- WaterIntake/WaterIntakeApp.swift
+++ WaterIntake/WaterIntakeApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct WaterIntakeApp: App {
     let persistenceController = PersistenceController.shared
 
-    
     var body: some Scene {
         WindowGroup {
             DashboardView()
